3f9969837c6ea0fc55de0cd68b61ff74e9af8049
Angshuman Ghosh <ghosh.angshuman1@gmail.com>
2023-05-02 14:37:15 +0530

    Added fish abbreviation and wezterm clear

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,21 @@ set fish_greeting ""
 
 set -gx EDITOR nvim
 
+# abbr
+abbr -a -- cpp 'clang++ -Wall -Wextra -g'
+abbr -a -- xx exit
+abbr -a -- du ncdu
+abbr -a -- cc clear
+abbr -a -- tt tmux
+abbr -a -- cf 'cd $HOME/.config/fish'
+abbr -a -- tb taskbook
+abbr -a -- cn 'cd $HOME/.config/nvim'
+abbr -a -- cw 'cd $HOME/.config/wezterm'
+abbr -a -- vi nvim
+abbr -a -- ys 'yay -Syu'
+abbr -a -- lg lazygit
+abbr -a -- cpp20 'clang++ -Wall -Wextra -std=c++20 -g'
+
 # DOTNET
 complete -f -c dotnet -a "(dotnet complete)"
 
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -46,8 +46,8 @@ config.window_background_opacity = 0.95
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = true
 config.window_padding = {
-    left = 0,
-    right = 0,
+    left = 5,
+    right = 5,
     top = 0,
     bottom = 0,
 }
@@ -75,6 +75,11 @@ config.keys = {
     { key = 'k', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-up') },
     { key = 'l', mods = 'CTRL', action = act.EmitEvent('ActivatePaneDirection-right') },
 
+    {
+        key = 'l',
+        mods = 'LEADER|CTRL',
+        action = act.SendKey { key = 'L', mods = 'CTRL' },
+    },
     {
         key = 'v',
         mods = 'LEADER',
